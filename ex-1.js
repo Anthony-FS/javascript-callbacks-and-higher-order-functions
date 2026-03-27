f823c882c2041b993e6feaa0907c104e8a681daf
AnthonyFS <anthonyfungesmith@gmail.com>
2026-03-27 15:20:37 +0700

    completed assignment

--- ex-1.js
+++ ex-1.js
@@ -1,17 +1,26 @@
 // Exercise #1: For Each Function
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
+
+//Callback Function
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000
 }
 
+//Higher Order Function
 function forEach(array, operation) {
   // Start coding here
+  const result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(operation(array[i]))
+  }
+  return result
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
@@ -19,8 +28,17 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+//function addSalary5000(previousSalary) {
+  return previousSalary + 5000
+}
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
-
+//function forEach(array, operation) {
+   const result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(operation(array[i]))
+  } 
+  return result
+} 
 ====================================
 */
